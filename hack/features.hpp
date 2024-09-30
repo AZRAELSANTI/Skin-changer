b2ea48d3914b5416d15b13c948e71306b4184f14
Fujiwara <83638372+AZRAELSANTI@users.noreply.github.com>
2024-09-30 14:06:48 +0300

    Update features.hpp

--- hack/features.hpp
+++ hack/features.hpp
@@ -1,1 +1,10 @@
+#pragma once
+#include "../../client/utilities/csgo.hpp"
+#include "visuals/visuals.h"
+#include "visuals/esp.h"
+#include "visuals/chams.h"
+#include "visuals/nadepred.h"
 
+#include "skins/skins.h"
+#include "skins/glove.h"
+#include "skins/parser.h"
